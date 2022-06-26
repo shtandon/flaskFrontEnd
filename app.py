a478dba82f4e88055892aff865f8c96567c1fca1
shtandon <52133102+shtandon@users.noreply.github.com>
2022-06-26 16:27:10 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,31 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for
 from Orchestrator.Orchestrator import *
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-   
-    return render_template('home.html')
-@app.route('/login')
-def login():
     username = request.form.get('username')
     password = request.form.get('password')
-    a = Orchestor(username, password)
-   # if a==1:
-    #     return redirect(url_for('success'))
-    #else:
-     #    return redirect(url_for('failur'))
-  #  return (username, password)
-    return render_template('login.html')
-   # Orchestor(questions)
-    
-@app.route('/success')
-def success():
-    return "Mails moved to folder"
-
-@app.route('/failure')
-def failur():
-    return "Wrong userid password"
-
+    Orchestor(username, password)
+    return render_template('home.html')
 
 if __name__ == '__main__':
    # Orchestor()
